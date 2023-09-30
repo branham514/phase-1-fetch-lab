74aaa612f32fcf155b4e52df2975271a612dca3c
branham514 <branham.gatheru@student.moringaschool.com>
2023-09-30 10:31:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+function fetchBooks(){ 
+ return fetch ('https://anapioficeandfire.com/api/books')
+ .then (res => res.json())
+ .then (renderBooks)
+  // Call renderBooks with the JSON data
 }
+
+  
 
 function renderBooks(books) {
   const main = document.querySelector('main');
